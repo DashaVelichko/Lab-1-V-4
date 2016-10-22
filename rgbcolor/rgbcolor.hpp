b99af7b76139ba1df32b3575130bd21f62a3eb9f
DashaVelichko <dasha.velichko@gmail.com>
2016-10-22 23:38:49 +0300

    Update rgbcolor.hpp

--- rgbcolor/rgbcolor.hpp
+++ rgbcolor/rgbcolor.hpp
@@ -12,7 +12,38 @@
 class RGBColor
 {
 
+public:
+int red,int green, int blue;
+unsigned int RGBPacked;
 
+ RGBColor (){
+ red = 255;
+ green = 255;
+ blue = 255;
+}
+RGBColor() {
+RGBPacked = 16777215;
+}
+getRed();
+{
+return 0
+}
+  
+getGreen();
+{
+return 0
+}
+
+getBlue();
+{
+return 0
+}
+
+getRGBPacked();
+{
+return 0
+}
+};
 
 };
 
