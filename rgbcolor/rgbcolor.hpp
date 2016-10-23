475c8b42e9b7e8e33edd44b2a4b98fd63840690a
Dashagit config --global user.name Dashagit commit -m first <dasha.velichko@gmail.com>
2016-10-23 22:36:32 +0300

    get&set

--- rgbcolor/rgbcolor.hpp
+++ rgbcolor/rgbcolor.hpp
@@ -16,45 +16,53 @@ class RGBColor
     unsigned int packedColor;
     public:
 
-RGBColor(char red_color, char blue_color, char green_color)// конструктор класса с тремя переменными 
+RGBColor(char red_color, char green_color, char blue_color)// конструктор класса с тремя переменными 
 
 {
     setRed(red_color); // вызов функции установки красного
 	setGreen (green_color); // вызов функции установки зеленого
 	setBlue (blue_color); // вызов функции установки синего
-	
-void setRGBColor (char red_color, char blue_color, char green_color) // установка цвета
-{
-   red=red_color; // инициализация красного
-   green=green_color; // инициализация зеленого
-   blue=blue_color; // инициализация синего
 }
-	char getRed(){
-        return red;
+RGBColor (unsigned int packed_Color); // конструктор класса с одной переменной 
+{
+	setpackedColor(packed_Color); // вызов функции установки запаковоного цвета
+}
+	void setRed (char red_color) {
+		red = red_color; // инициализация красного
+	}
+	void setGreen (char green_color) {
+		green = green_color; // инициализация зеленого
+	}
+	void setBlue (char blue_color) {
+		blue = blue_color; // инициализация синего
+	}
+	
+    void packedColor (unsigned int packed_Color) {
+		packedColor = packed_Color; // инициализация packedColor
+	}
+	 
+	 
+	char getRed(){ 
+        return red; // доступ к private переменной red
 	} 
 	char getGreen(){
-        return green;
+        return green; // доступ к private переменной green
 	} 
 	
 	char getBlue(){
-        return blue;
+        return blue; // доступ к private переменной blue
 	} 
 	
-}
 
-RGBColor (unsigned int packed_Color); // конструктор класса с одной переменной 
-{
-	setRGBColor(packed_Color); // вызов функции установки запаковоного цвета
-	
-    void setRGBColor (unsigned int packed_Color) // установка цвета
- 
-    unsigned int getPackedRGB(){
-         return white;
+
+
+    unsigned int getpackedColor(){
+         return packedColor; // доступ к private переменной packedColor
 }
 	
 };
 
-};
+
 
 
 /*****************************************************************************/
