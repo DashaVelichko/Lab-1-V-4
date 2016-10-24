f095c189f23b1392d3018195058089c7ed8c2d9a
Dashagit config --global user.name Dashagit commit -m first <dasha.velichko@gmail.com>
2016-10-24 11:03:14 +0300

    return fun

--- rgbcolor/rgbcolor.hpp
+++ rgbcolor/rgbcolor.hpp
@@ -16,40 +16,46 @@ class RGBColor
     unsigned int packedColor;
     public:
 
-RGBColor(char red_color, char green_color, char blue_color)// конструктор класса с тремя переменными 
+RGBColor(unsigned char  red_color, unsigned char  green_color, unsigned char  blue_color)// конструктор класса с тремя переменными 
 
 {
     setRed(red_color); // вызов функции установки красного
 	setGreen (green_color); // вызов функции установки зеленого
 	setBlue (blue_color); // вызов функции установки синего
+	setpacked_color (convert_RGBColor);
 }
-RGBColor (unsigned int packed_color); // конструктор класса с одной переменной 
+RGBColor (unsigned int packed_color) // конструктор класса с одной переменной 
 {
 	setPackedRGB(packed_color); // вызов функции установки запаковоного цвета
 }
-	void setRed (char red_color) {
+	void setRed (unsigned char  red_color) {
 		red = red_color; // инициализация красного
 	}
-	void setGreen (char green_color) {
+	void setGreen (unsigned char  green_color) {
 		green = green_color; // инициализация зеленого
 	}
-	void setBlue (char blue_color) {
+	void setBlue (unsigned char  blue_color) {
 		blue = blue_color; // инициализация синего
 	}
 	
     void setPackedRGB (unsigned int packed_color) {
 		packedColor = packed_color; // инициализация packedColor
 	}
+	void setpacked_color (unsigned int convert_RGBColor) {
+		packed_color = convert_RGBColor;
+	}
+	 return convert_RGBColor () {
+		 convert_RGBColor = ((red * 65536) + (green * 256) + blue);
+	 }
 	 
-	 
-	char getRed(){ 
+	unsigned char  getRed(){ 
         return red; // доступ к private переменной red
 	} 
-	char getGreen(){
+	unsigned char  getGreen(){
         return green; // доступ к private переменной green
 	} 
 	
-	char getBlue(){
+	unsigned char  getBlue(){
         return blue; // доступ к private переменной blue
 	} 
 	
@@ -59,6 +65,9 @@ RGBColor (unsigned int packed_color); // конструктор класса с 
     unsigned int getPackedRGB(){
          return packedColor; // доступ к private переменной packedColor
 }
+	
+	
+	
 	
 };
 
