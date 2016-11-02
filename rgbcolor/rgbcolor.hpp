44c43eb69bb2c97adf452258090b7e0d89c80abf
DashaVelichko <dasha.velichko@gmail.com>
2016-11-02 17:03:51 +0200

    new func

--- rgbcolor/rgbcolor.hpp
+++ rgbcolor/rgbcolor.hpp
@@ -5,72 +5,71 @@
 
 /*****************************************************************************/
 
-#include <iostream>
+#include <iostream> 
 
 /*****************************************************************************/
 
 class RGBColor
 {
-    private:
-    char red,green,blue;
-    unsigned int packedColor;
-    public:
+private:
+	char red, green, blue;
+	unsigned int packedColor;
+public:
 
-RGBColor(unsigned char  red_color, unsigned char  green_color, unsigned char  blue_color)// конструктор класса с тремя переменными 
+	RGBColor(unsigned char red_color, unsigned char green_color, unsigned char blue_color)// конструктор класса с тремя переменными 
 
-{
-    setRed(red_color); // вызов функции установки красного
-	setGreen (green_color); // вызов функции установки зеленого
-	setBlue (blue_color); // вызов функции установки синего
-	setpacked_color (convert_RGBColor);
-}
-RGBColor (unsigned int packed_color) // конструктор класса с одной переменной 
-{
-	setPackedRGB(packed_color); // вызов функции установки запаковоного цвета
-}
-	void setRed (unsigned char  red_color) {
-		red = red_color; // инициализация красного
+	{
+		setRed(red_color); // вызов функции установки красного 
+		setGreen(green_color); // вызов функции установки зеленого 
+		setBlue(blue_color); // вызов функции установки синего 
 	}
-	void setGreen (unsigned char  green_color) {
-		green = green_color; // инициализация зеленого
+	RGBColor(unsigned int packed_color) // конструктор класса с одной переменной 
+	{
+		setPackedRGB(packed_color); // вызов функции установки запаковоного цвета 
 	}
-	void setBlue (unsigned char  blue_color) {
-		blue = blue_color; // инициализация синего
+	void setRed(unsigned char red_color) {
+		red = red_color; // инициализация красного 
 	}
-	
-    void setPackedRGB (unsigned int packed_color) {
-		packedColor = packed_color; // инициализация packedColor
+	void setGreen(unsigned char green_color) {
+		green = green_color; // инициализация зеленого 
 	}
-	void setpacked_color (unsigned int convert_RGBColor) {
-		packed_color = convert_RGBColor;
+	void setBlue(unsigned char blue_color) {
+		blue = blue_color; // инициализация синего 
 	}
-	 return convert_RGBColor () {
-		 convert_RGBColor = ((red * 65536) + (green * 256) + blue);
-	 }
-	 
-	unsigned char  getRed(){ 
-        return red; // доступ к private переменной red
-	} 
-	unsigned char  getGreen(){
-        return green; // доступ к private переменной green
-	} 
-	
-	unsigned char  getBlue(){
-        return blue; // доступ к private переменной blue
-	} 
-	
 
+	void setPackedRGB(unsigned int packed_color) {
+		packedColor = packed_color; // инициализация packedColor 
+	}
+
+	unsigned int packed_color() {
+
+		packed_color = ((red * 65536) + (green * 256) + blue);
+
+		return packed_color;
+	}
+
+	unsigned char getRed() {
+		return red; // доступ к private переменной red 
+	}
+	unsigned char getGreen() {
+		return green; // доступ к private переменной green 
+	}
+
+	unsigned char getBlue() {
+		return blue; // доступ к private переменной blue 
+	}
 
 
-    unsigned int getPackedRGB(){
-         return packedColor; // доступ к private переменной packedColor
-}
-	
-	
-	
-	
+
+
+	unsigned int getPackedRGB() {
+		return packedColor; // доступ к private переменной packedColor 
+	}
+
+
+
+
 };
-
 
 
 
